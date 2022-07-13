6a6b021150807b10b272a62dcd5b650da3a2ba8a
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-07-12 21:54:13 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
+import NotificationButton from "./components/NotificationButton";
+
+//no react não faz uso da palavra class
+//O react não permite exportar duplas de parametros coloque o <></>
+//Sempre Colocar em pascal case
+//App tsx é um componente principal do projeto
 function App() {
-  return (<h1>ola mundo</h1>)
+  return (<NotificationButton/>);
 }
-export default App
+export default App;
