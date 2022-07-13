ded8cfecb4cc6eff07edb8a0c8873ee57f57592c
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-07-12 23:18:05 -0300

    Outros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import NotificationButton from "./components/NotificationButton";
 
+import Header from "./components/Header/index";
+import SalesCard from "./components/SalesCard";
+// O main e o section fica sendo da página
 //no react não faz uso da palavra class
 //O react não permite exportar duplas de parametros coloque o <></>
 //Sempre Colocar em pascal case
 //App tsx é um componente principal do projeto
 function App() {
-  return (<NotificationButton/>);
+  return (<><Header/>
+  <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+            <SalesCard></SalesCard>
+        </div>
+        </section>
+        </main>
+        </>);
 }
 export default App;
